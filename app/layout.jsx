d105de87d3505e519e4e7b74822ca08f49df5954
razaabbas112 <160321439+razaabbas112@users.noreply.github.com>
2026-09-25 13:33:38 +0500

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Raza Session Generator',
+  description: 'Raza WhatsApp Session Generator'
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
